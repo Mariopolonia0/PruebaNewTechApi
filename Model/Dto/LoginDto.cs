91c7dac38818e4c61e17c037add3a5f2b8a6a667
mario <mariopolonia0@gmil.com>
2024-05-16 21:51:54 -0400

    ya si

---
+++ Model/Dto/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PruebaNewTechApi.Model.Dto
+{
+    public class LoginDto
+    {
+        public int estudianteId { get; set; }
+        public string nombreEstudiante { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+
+        public LoginDto(int estudianteId, string nombres, string password)
+        {
+            this.estudianteId = estudianteId;
+            nombreEstudiante = nombres;
+            this.password = password;
+        }
+    }
+}
---
+++ Model/Dto/LoginIn.cs
@@ -0,0 +1,14 @@
+﻿namespace PruebaNewTechApi.Model.Dto
+{
+    public class LoginIn
+    {
+        public string nombreEstudiante { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+
+        public LoginIn(int estudianteId, string nombres, string password)
+        {
+            nombreEstudiante = nombres;
+            this.password = password;
+        }
+    }
+}
