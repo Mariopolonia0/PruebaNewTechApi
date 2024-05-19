58605c83d6b37f2dae2fb80a06c4501660fab793
mario <mariopolonia0@gmil.com>
2024-05-19 13:11:02 -0400

    ya si

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using PruebaNewTechApi.Model;
+using PruebaNewTechApi.Model.ControlTarea;
 
 namespace PruebaNewTechApi.DAL
 {
@@ -9,6 +10,7 @@ namespace PruebaNewTechApi.DAL
     {
         public DbSet<Book>? Book { set; get; }
         public DbSet<Usuario>? Usuarios { set; get; }
+        public DbSet<Tarea>? Tareas { set; get; }
 
         public Contexto(DbContextOptions<Contexto> options) : base(options)
         {
@@ -25,30 +27,6 @@ namespace PruebaNewTechApi.DAL
                 NombreAutor = "Grabriel Garcia Marquez",
                 Edicion = "Tercera",
                 Precio = 1000
-            });
-            modelBuilder.Entity<Book>().HasData(new Book
-            {
-                BookId = 101,
-                Nombre = "El lenguaje de programacion C#",
-                NombreAutor = "Francisco Javier Ceballos",
-                Edicion = "Primera",
-                Precio = 10000
-            });
-            modelBuilder.Entity<Book>().HasData(new Book
-            {
-                BookId = 103,
-                Nombre = "Kotlin in Action",
-                NombreAutor = "Dmitry Jemerov and Svetlana Isakova",
-                Edicion = "Tercera",
-                Precio = 13000
-            });
-            modelBuilder.Entity<Book>().HasData(new Book
-            {
-                BookId = 104,
-                Nombre = "Java 2 Manual de programacion",
-                NombreAutor = "Luis Joyanes Aguilar",
-                Edicion = "Tercera",
-                Precio = 9000
             });
             modelBuilder.Entity<Usuario>().HasData(new Usuario
             {
---
+++ Model/ControlTarea/Tarea.cs
@@ -0,0 +1,13 @@
+﻿namespace PruebaNewTechApi.Model.ControlTarea
+{
+    public class Tarea
+    {
+        public int tareaId { get; set; }
+        public int usuarioId { get; set; }
+        public string descripcion { get; set; }
+        public String fechaCreada { get; set; }
+        public string fechaTerminada { get; set; }
+        public string fechaVecimineto { get; set; }
+        public string estado { get; set; }
+    }
+}
