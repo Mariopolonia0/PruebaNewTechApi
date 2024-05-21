20de93a268bced029fb14af81adeab88a9f6b4a8
mario <mariopolonia0@gmil.com>
2024-05-20 20:00:28 -0400

    agrengdando nuevo controlador

---
+++ Controllers/UsuarioControllers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PruebaNewTechApi.DAL;
+using PruebaNewTechApi.Model;
+
+namespace PruebaNewTechApi.Controllers
+{
+    [Route("Usuario")]
+    [ApiController]
+    public class UsuarioControllers : ControllerBase
+    {
+        private readonly Contexto _context;
+        public UsuarioControllers(Contexto context)
+        {
+            _context = context;
+        }
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuario!.Any(e => e.UsuarioId == id);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetListUsuario()
+        {
+            return await _context.Usuario!.ToListAsync();
+        }
+    }
+}
